1e89ea340eea9961a459e8e1f24f34ee1c46f89f
Jorrit van der Ven <j.vander.ven@portbase.com>
2024-01-18 21:30:17 +0100

    Welcome to 2024...

--- src/main/webapp/src/app/app.component.ts
+++ src/main/webapp/src/app/app.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import {HttpClient} from "@angular/common/http";
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Hello World Application';
   name: string = 'name';
-  message = null;
+  message: String|null = null;
 
-  constructor(private http : HttpClient) {
-
-  }
+  constructor(private http : HttpClient) { }
 
   sayHello() : void {
     this.http.get<HelloResponse>('hello/' + this.name)
